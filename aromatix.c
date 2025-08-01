b84fb1fdc4d6cc0ac01fc77c614629836e2e1be3
Tsapelis George <tsapelis.george@gmail.com>
2025-08-02 00:46:54 +0300

    The menu function has been added

--- aromatix.c
+++ aromatix.c
@@ -16,7 +16,32 @@ struct fragrance{
     char gender[50];    
 };
 
+int menu(){
+  int e;
+  do{ 
+    printf("Welcome to the Aromatix Fragrance Management System\n");
+    printf("1. Add a new fragrance\n");
+    printf("2. Edit a fragrance\n");
+    printf("3. Search a fragrance by brand\n");
+    printf("4. Search by notes\n");
+    printf("5. Search perfumers\n");
+    printf("6. Search award winning fragrances\n");
+    printf("7. Display all fragrances\n");
+    printf("8. Display fragrances by year\n");
+
+
+    printf("Dvse thn epilogh:");
+    scanf("%d", &e);
+  }while (e < 1 || e > 8);
+
+  return e;
+}    
+
 
 int main(){
-    return 0;
+  int ep;
+  
+  ep = menu();
+  return 0;
 }
+
